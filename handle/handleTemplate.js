feef697e52a23c9ece2e0e3fd7b516b5c94632be
Aomd <g00665@163.com>
2021-05-10 10:00:17 +0800

    添加清理 zip文件 修改 gzip 生成文件后缀
    添加清理 zip文件 修改 gzip 生成文件后缀

--- handle/handleTemplate.js
+++ handle/handleTemplate.js
@@ -5,11 +5,13 @@ import { mode } from "../handle";
 
 // 开启资源gzip
 import gzip from 'gulp-gzip';
+import del from "del";
 
 // 替换文件hash
 const revCollector = require('gulp-rev-collector');
 
 function handleTemplate() {
+
   console.log('替换模板hash')
   var item = src(['./.config/json/*.json'].concat(config.src.template))
     .pipe(revCollector())
@@ -17,13 +19,17 @@ function handleTemplate() {
 
   // 是build 就加 zip
   if (mode == 'build') {
+    // 清理css gzip
+    del(`${config.dest.css}*.css.zip`)
     console.log('生成 css gzip文件')
     src([`${config.dest.css}*.css`])
-      .pipe(gzip())
+      .pipe(gzip({ extension: 'zip' }))
       .pipe(dest(config.dest.css))
+
+    del(`${config.dest.js}*.js.zip`)
     console.log('生成 js gzip文件')
     src([`${config.dest.js}*.js`])
-      .pipe(gzip())
+      .pipe(gzip({ extension: 'zip' }))
       .pipe(dest(config.dest.js))
 
   }
